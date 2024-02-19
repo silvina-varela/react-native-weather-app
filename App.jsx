1be2c5782e25a84ac6cce73c36a620c2465bb8ed
Silvina Varela <varelasilvina@gmail.com>
2024-02-19 20:57:41 -0300

    build

--- App.jsx
+++ App.jsx
@@ -1,45 +1,17 @@
 import React from "react";
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./src/components/Tabs";
-import { useGetWeather } from "./src/hooks/useGetWeather";
-import ErrorItem from "./src/components/ErrorItem";
-const App = () => {
-  const [loading, error, weather] = useGetWeather();
+import Main from "./src/components/Main";
+import { WeatherProvider } from "./src/hooks/useWeather";
 
-  if (weather && weather.list && !loading) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <Tabs weather={weather} />
-        </NavigationContainer>
-      </SafeAreaView>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {error ? (
-          <ErrorItem />
-        ) : (
-          <ActivityIndicator size={"large"} color={"blue"} />
-        )}
-      </View>
-    );
-  }
+const App = () => {
+
+
+ return(
+  <WeatherProvider>
+    <Main/>
+  </WeatherProvider>
+ )
 };
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-});
+
 
 export default App;
